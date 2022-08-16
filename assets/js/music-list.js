5351f91abee853197752feace22c764cf28b4853
Eslam20000 <esla200000@gmail.com>
2022-08-16 05:26:15 +0300

    first commit

---
+++ assets/js/music-list.js
@@ -0,0 +1,12 @@
+ //       <!-- made by Islam Tafesh +970592584234-->
+
+let allMusic =[
+    {
+        name:"Different World",
+        artist: "Alan Walker",
+        img:"Alan_Walker_Different_World",
+        src:"Alan_Walker_Different_World"
+    }   
+    
+    
+]
