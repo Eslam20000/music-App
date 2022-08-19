5075ccc5322f39c3b58ccec557e5510f9d4adaea
Eslam20000 <esla200000@gmail.com>
2022-08-19 12:21:49 +0300

    edit 4

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -139,13 +139,11 @@ const wrapper = document.querySelector(".wrapper"),
 
     /* Here the code of make full screen when click at full screen btn */
 
-    var elem = document.getElementById("full_sec");
-    function openFullscreen() {
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-      }
-    }
+    var goFS = document.getElementById('goFS');
+    goFS.addEventListener(
+      'click',
+      function () {
+        document.body.requestFullscreen();
+      },
+      false,
+    );
